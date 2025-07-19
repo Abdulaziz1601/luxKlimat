b42bf96d517e671fe5c8ad5818a4432ffa0be2cd
Abdullaev Abdulaziz <aabdullaev@lincolnlabs.co>
2025-07-19 17:06:49 +0500

    feat: remove preloader feature from from website and replace not working photos with ones that are working

--- js/script.js
+++ js/script.js
@@ -23,7 +23,6 @@
 			captcha: $('.recaptcha'),
 			copyrightYear: $(".copyright-year"),
 			isotope: $(".isotope-wrap"),
-			preloader: $(".preloader"),
 			rdNavbar: $(".rd-navbar"),
 			rdMailForm: $(".rd-mailform"),
 			rdInputLabel: $(".form-label"),
@@ -44,29 +43,8 @@
 
 	// Initialize scripts that require a loaded page
 	$window.on('load', function () {
-		// Page loader & Page transition
-		if (plugins.preloader.length && !isNoviBuilder) {
-			pageTransition({
-				target: document.querySelector('.page'),
-				delay: 0,
-				duration: 100,
-				classIn: 'fadeIn',
-				classOut: 'fadeOut',
-				classActive: 'animated',
-				conditions: function (event, link) {
-					return !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link) && !event.currentTarget.hasAttribute('data-lightgallery');
-				},
-				onTransitionStart: function (options) {
-					setTimeout(function () {
-						plugins.preloader.removeClass('loaded');
-					}, options.duration * .75);
-				},
-				onReady: function () {
-					plugins.preloader.addClass('loaded');
-					windowReady = true;
-				}
-			});
-		}
+		// Set window ready immediately since no preloader
+		windowReady = true;
 
 		// jQuery Count To
 		if (plugins.counter.length) {
